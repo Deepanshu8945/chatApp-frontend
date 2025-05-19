08ed9ee9d2397bd6ce5a09d21b16d4d041749d48
Deepanshu8945 <Deepanshu010101@gmail.com>
2025-05-19 09:32:48 +0530

    changes of backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"? "http://localhost:5001/api" : "/api",
+    baseURL: "https://chatapp-backend-o1br.onrender.com",
     withCredentials:true,
 })
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,10 +1,9 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios.js"
 import toast from "react-hot-toast"
-import { LucideFileSpreadsheet } from "lucide-react"
 import {io} from "socket.io-client"
 
-const BASE_URL = import.meta.env.MODE === "development"? "http://localhost:5001" : "/";
+const BASE_URL = "https://chatapp-backend-o1br.onrender.com";
 
 export const useAuthStore = create((set,get) => ({
     authUser:null,
