e3b383cc6657c52ea08bd3694b34b95a1d0ed6bf
Deepanshu8945 <Deepanshu010101@gmail.com>
2025-05-19 09:42:57 +0530

    fixes

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chatapp-backend-o1br.onrender.com",
+    baseURL: "https://chatapp-backend-o1br.onrender.com/api",
     withCredentials:true,
 })
